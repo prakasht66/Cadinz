3b6f2891d4451b971489f94a9c540485df9264e9
MaPr <prakasht66@gmail.com>
2019-07-04 22:42:56 +0530

    Play Store Release 1.0

---
+++ app/src/main/java/app/hamcr7/mapr/cadonz/ShowInfo.java
@@ -0,0 +1,30 @@
+package app.hamcr7.mapr.cadonz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ShowInfo extends AppCompatActivity {
+
+    TextView titleView;
+
+    TextView contentView;
+
+    String title,description;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_info);
+
+        titleView=findViewById(R.id.title_showInfo);
+        contentView=findViewById(R.id.show_InfoText);
+
+        Bundle bundle = getIntent().getExtras();
+        title= bundle.getString("Title");
+        description=bundle.getString("Content");
+
+        titleView.setText(title);
+        contentView.setText(description);
+    }
+}
